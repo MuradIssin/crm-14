2b94ac2493900f3fad116ddfe3ea79decb5d228a
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-05-29 18:42:54 +0600

    Create task03.js

---
+++ task03.js
@@ -0,0 +1,20 @@
+'use strict';
+console.warn('Модуль 4 Урок 11 -this Задача#1');
+
+const randonTen = (ten = 10) => Math.round(Math.random() * ten);
+
+function foo(numIncome = []) {
+  // console.log(numIncome);
+  numIncome.push(randonTen())
+  if (numIncome.reduce((acc, item) => { return acc + item }, 0) >= 50) {
+    // console.log(numIncome);
+    // return [1, 2, 3]
+    return numIncome;
+  }
+  return foo(numIncome);
+}
+
+// console.log(randonTen())
+const arr = foo([])
+// console.log(`задача #2 - ${foo([])}`);
+console.log(arr);
