f2d73054e751b095a7d723398e2771f007d0cfe6
MuradIssin <117025448+MuradIssin@users.noreply.github.com>
2023-06-01 10:51:41 +0600

    Update task03.js

--- task03.js
+++ task03.js
@@ -4,17 +4,9 @@ console.warn('Модуль 4 Урок 11 -this Задача#1');
 const randomTen = (ten = 10) => Math.round(Math.random() * ten);
 
 function foo(numIncome = []) {
-  // console.log(numIncome);
   numIncome.push(randomTen())
-  if (numIncome.reduce((acc, item) => { return acc + item }, 0) >= 50) {
-    // console.log(numIncome);
-    // return [1, 2, 3]
-    return numIncome;
-  }
+  if (numIncome.reduce((acc, item) => { return acc + item }, 0) >= 50) return numIncome;
   return foo(numIncome);
 }
-
-// console.log(randonTen())
 const arr = foo([])
-// console.log(`задача #2 - ${foo([])}`);
 console.log(arr);
