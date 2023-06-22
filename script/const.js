82fb6e768fdb67cf4c345a37df5b2854e32f9344
Murad Isin <murad.78@mail.ru>
2023-06-22 17:03:02 +0600

    промежуточное решение

---
+++ script/const.js
@@ -0,0 +1,15 @@
+const modalTitle = document.querySelector('.modal__title').
+  innerHTML;
+const modalForm = document.querySelector('.modal__set').innerHTML;
+const modalCheckDiscount = document.querySelector
+  ('.set__checkbox');
+const modalTextDiscount = document.querySelector
+  ('.set__input-discount');
+const modalTotalAmount = document.querySelector
+  ('.set__total-amount');
+
+
+console.log(modalTitle, modalCheckDiscount);
+console.log(modalCheckDiscount.checked);
+console.log(modalTextDiscount.innerHTML);
+console.log(modalTotalAmount.innerHTML);
