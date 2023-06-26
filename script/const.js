b89c75e095e13958dd9be223dfdf111268b67809
Murad Isin <murad.78@mail.ru>
2023-06-26 09:16:54 +0600

    add modal

--- script/const.js
+++ script/const.js
@@ -1,6 +1,6 @@
 const modalTitle = document.querySelector('.modal__title').
   innerHTML;
-const modalForm = document.querySelector('.modal__set').innerHTML;
+const modalForm = document.querySelector('.modal__form');
 const modalCheckDiscount = document.querySelector
   ('.set__checkbox');
 const modalTextDiscount = document.querySelector
@@ -19,4 +19,5 @@ console.log(modalTitle, modalCheckDiscount);
 console.log(modalCheckDiscount.checked);
 console.log(modalTextDiscount.innerHTML);
 console.log(modalTotalAmount.innerHTML);
-console.log(modalId.innerHTML)
+console.log(modalId.innerHTML);
+console.log(modalForm)
